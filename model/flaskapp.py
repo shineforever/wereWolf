38675bb021ba1a4e52252447c472f9dea2b78189
1126978661@qq.com <1126978661@qq.com>
2017-09-12 11:25:37 +0800

    [descriptions] 1, 狼人杀优化2.0 , 使用Python3

--- model/flaskapp.py
+++ model/flaskapp.py
@@ -4,5 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost:3306/werewolfwechat?charset=utf8mb4'
+# app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost:3306/werewolfwechat?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://yz:yz8833757992@@rm-uf6lef7znkjk2kdl2o.mysql.rds.aliyuncs.com:3306/werewolfwechat?charset=utf8mb4'
 db = SQLAlchemy(app)
